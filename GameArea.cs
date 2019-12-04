90fb04e4ccaa0f35549f4cc5048ec6183f719dc7
David <dusek.d@seznam.cz>
2019-12-04 23:58:23 +0100

    Edited some attributes

--- GameArea.cs
+++ GameArea.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
-namespace ConsoleSnake {
+namespace Snake {
 	class GameArea {
-		public int lenX;
-		int lenY;
-		string[,] matrix;
-		string empty = "XX";
-		string snakeBody = "()";
-		string snakeHead = ".<>";
+		public int lenX { get; private set; }
+		public int lenY { get; private set; }
+		public string[,] matrix { get; set; }
+		public string empty = "##";
+		public string snakeBody = "()";
+		public string snakeHead = "<>";
+		public string candy = "*";
 
 		public void SetConsoleSize() {
 			Console.SetWindowSize(lenX*2, lenY+1);
 		}
+
 		public GameArea(int lenX, int lenY) {
 			this.lenX = lenX;
 			this.lenY = lenY;
@@ -25,12 +27,20 @@ namespace ConsoleSnake {
 		public void InitMatrix() {
 			for (int x = 0; x < lenX; x++) {
 				for (int y = 0; y < lenY; y++) {
+					if (y == lenY/2 && x == lenX / 2) {
+						matrix[x, y] = snakeHead;
+						continue;
+					}
 					matrix[x, y] = empty;
 				}
 			}
 		}
 		public void ChangeMatrix() {
-			
+			for (int x = 0; x < lenX; x++) {
+				for (int y = 0; y < lenY; y++) {
+
+				}
+			}
 		}
 
 		public void ShowMatrix() {
--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 
-namespace ConsoleSnake {
+namespace Snake {
 	class Program {
 		static void Main(string[] args) {
 			GameArea gameArea = new GameArea(15,15);
 			gameArea.SetConsoleSize();
-			gameArea.ShowEveryXSeconds(1);
+			Movement movement = new Movement();
+			movement.GetNeighbors(gameArea, 0, 10);
+			Tuple<bool, bool, bool, bool> x = movement.CheckWalls();
+			Console.WriteLine(x);
+			//gameArea.ShowEveryXSeconds(1);
 		}
 	}
 }
