a61d79d1d1c0884ddcf2dc41d3a0bb906a123d21
David Dusek <dusek.d@seznam.cz>
2019-12-08 00:55:40 +0100

    needed methods for snake parts locations

--- GameArea.cs
+++ GameArea.cs
@@ -5,48 +5,57 @@ using System.Threading;
 
 namespace Snake {
 	class GameArea {
-		public int lenX { get; private set; }
-		public int lenY { get; private set; }
-		public string[,] matrix { get; set; }
-		public string empty = "##";
-		public string snakeBody = "()";
-		public string snakeHead = "<>";
-		public string candy = "*";
+		public int LenX { get; private set; }
+		public int LenY { get; private set; }
+		public string[,] Matrix { get; set; }
+
+		public string Empty { get; }
+		public string SnakeBody { get; }
+		public string SnakeTail { get; }
+		public string SnakeHead { get; }
+		public string Candy { get; }
+		public string Wall { get; }
+
+		public int speedMs { get; }
+		
+		public Tuple<int, int> SnakeHeadLocation { get; private set; }
+		public Tuple<int, int> SnakeTailLocation { get; private set; }
 
 		public void SetConsoleSize() {
-			Console.SetWindowSize(lenX*2, lenY+1);
+			Console.SetWindowSize(LenX*2, LenY+1);
 		}
 
 		public GameArea(int lenX, int lenY) {
-			this.lenX = lenX;
-			this.lenY = lenY;
-			matrix = new string[lenX, lenY];
+			this.LenX = lenX;
+			this.LenY = lenY;
+			Matrix = new string[lenX, lenY];
+			Empty = "##";
+			SnakeBody = "()";
+			SnakeHead = "<>";
+			SnakeTail = "{}";
+			Candy = "*";
+			Wall = "[]";
+			speedMs = 1000;
 			InitMatrix();
 		}
 
 		public void InitMatrix() {
-			for (int x = 0; x < lenX; x++) {
-				for (int y = 0; y < lenY; y++) {
-					if (y == lenY/2 && x == lenX / 2) {
-						matrix[x, y] = snakeHead;
+			for (int x = 0; x < LenX; x++) {
+				for (int y = 0; y < LenY; y++) {
+					if (y == LenY/2 && x == LenX / 2) {
+						Matrix[x, y] = SnakeHead;
 						continue;
 					}
-					matrix[x, y] = empty;
-				}
-			}
-		}
-		public void ChangeMatrix() {
-			for (int x = 0; x < lenX; x++) {
-				for (int y = 0; y < lenY; y++) {
-
+					Matrix[x, y] = Empty;
 				}
 			}
 		}
 
+		//show matrix in console
 		public void ShowMatrix() {
-			for (int y = 0; y < lenY; y++) {
-				for (int x = 0; x < lenX; x++) {
-					Console.Write(matrix[x, y]);
+			for (int y = 0; y < LenY; y++) {
+				for (int x = 0; x < LenX; x++) {
+					Console.Write(Matrix[x, y]);
 				}
 				Console.WriteLine();
 			}
@@ -64,9 +73,18 @@ namespace Snake {
 			}
 
 		}
-
-		public int GetSnakeLocation() {
-			return 0;
+		//update head and tail locations
+		public void UpdateSnakeLocation() {
+			for (int x = 0; x < LenX; x++) {
+				for (int y = 0; y < LenY; y++) {
+					if(Matrix[x,y] == SnakeHead) {
+						SnakeHeadLocation = Tuple.Create(x, y);
+					}
+					if(Matrix[x,y] == SnakeTail) {
+						SnakeTailLocation = Tuple.Create(x, y);
+					}
+				}
+			}
 		}
 	}
 }
